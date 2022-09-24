932e4526d3df53ea412c883137cda4fcf26a2205
EkaterinaVikt <kat.abramova@gmail.com>
2022-09-24 16:29:05 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -135,6 +135,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -143,5 +147,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearcResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
